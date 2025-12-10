c5451190d4e54d8fc73e447e4fe9f3c1df7e5cfd
yjinnn0719 <yjinn0719@elicer.com>
2025-12-10 19:13:53 +0900

    그래픽을 귀엽게 개선 (이슈 #1 해결)

--- snake.js
+++ snake.js
@@ -29,19 +29,55 @@ function collision(head, arr) {
     return false;
 }
 
+function drawSnakePart(x, y, isHead) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x + box / 2, y + box / 2, box / 2 - 2, 0, Math.PI * 2);
+    ctx.fillStyle = isHead ? '#ffe066' : '#b2f2ff';
+    ctx.shadowColor = isHead ? '#ffd700' : '#69dbff';
+    ctx.shadowBlur = isHead ? 12 : 6;
+    ctx.fill();
+    ctx.restore();
+    // 눈 그리기 (머리만)
+    if (isHead) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(x + box / 2 - 4, y + box / 2 - 3, 2, 0, Math.PI * 2);
+        ctx.arc(x + box / 2 + 4, y + box / 2 - 3, 2, 0, Math.PI * 2);
+        ctx.fillStyle = '#333';
+        ctx.fill();
+        ctx.restore();
+    }
+}
+
+function drawFood(x, y) {
+    // 귀여운 사과
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x + box / 2, y + box / 2, box / 2 - 2, 0, Math.PI * 2);
+    ctx.fillStyle = '#ff6f61';
+    ctx.shadowColor = '#ffb3ab';
+    ctx.shadowBlur = 8;
+    ctx.fill();
+    // 잎사귀
+    ctx.beginPath();
+    ctx.arc(x + box / 2, y + box / 2 - 7, 3, 0, Math.PI, true);
+    ctx.fillStyle = '#51cf66';
+    ctx.fill();
+    ctx.restore();
+}
+
 function draw() {
-    ctx.fillStyle = '#111';
+    ctx.fillStyle = '#e3fafc';
     ctx.fillRect(0, 0, canvasSize, canvasSize);
 
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = i === 0 ? '#4caf50' : '#8bc34a';
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        drawSnakePart(snake[i].x, snake[i].y, i === 0);
     }
 
-    ctx.fillStyle = '#f44336';
-    ctx.fillRect(food.x, food.y, box, box);
+    drawFood(food.x, food.y);
 
-    ctx.fillStyle = '#fff';
+    ctx.fillStyle = '#495057';
     ctx.font = '20px Arial';
     ctx.fillText('Score: ' + score, 10, 390);
 
@@ -68,7 +104,7 @@ function draw() {
         collision(newHead, snake)
     ) {
         clearInterval(gameInterval);
-        ctx.fillStyle = '#fff';
+        ctx.fillStyle = '#ff8787';
         ctx.font = '40px Arial';
         ctx.fillText('Game Over', 80, 200);
         return;
